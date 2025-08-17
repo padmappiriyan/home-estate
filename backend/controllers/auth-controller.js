1caa6e3466367745cad39e021e54fa713f8113c5
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-17 23:24:30 +0530

    implement user sign-up functionality with bcrypt hashing and create auth routes

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+import User from "../models/user-model.js";
+
+export const signUp =async (req,res)=>{
+     console.log("sign up");
+      const {username,email,password}=req.body;
+      console.log(req.body);
+      const hasPassword=bcrypt.hashSync(password,10);
+      const newUser= new User({username,email,password:hasPassword});
+      try{
+          await newUser.save();
+          res.json({
+        status:"success",
+        message:"User created successfully"
+      })
+      }
+      catch(error){
+        res.json({
+            status:"failed",
+            message:"error.message"
+        })
+      }
+      
+}
--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,19 @@ import express from 'express';
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth-route.js";
 
 const app=express();
+connectDB();
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
 
-connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.use("/api/auth",authRoutes);
+
+
