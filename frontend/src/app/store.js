f7ba88178fd63fcc89cf78833db9e5cf04860104
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-20 22:43:14 +0530

    Add Redux Persist for state management and update main entry point

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  userReducer from '../features/userSlice'
-export default configureStore({
-  reducer: {user:userReducer},
-  middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
-    serializableCheck:false
-  })
-})
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from '../features/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+const persistConfig = {
+  key: 'root', 
+  version: 1,
+  storage,
+  
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); 
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+});
+
+export const persistor = persistStore(store);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import  store from './app/store.js'
+import App from './App' 
+import { store, persistor } from './app/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-createRoot(document.getElementById('root')).render(
-<Provider store={store}>
-      <App />
-</Provider>
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App /> 
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 )
