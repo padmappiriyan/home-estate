17357f7e9b10352c1bb74323f26775fda415944c
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-23 12:17:05 +0530

    Add Profile_card component to display user profile information in Profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Profile_form from '../components/Profile_form';
+import Profile_card from '../components/Profile_card';
 
 export default function Profile() {
   return (
@@ -13,6 +14,9 @@ export default function Profile() {
       
       <div className="absolute top-1/2 left-[18px] w-1/2 h-1/3">
         <Profile_form />
+      </div>
+      <div className='absolute top-1/2 right-[18px] w-1/2 h-1/3'>
+      <Profile_card />
       </div>
     </div>
   );
