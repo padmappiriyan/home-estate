daf80aec5d9a6d1874e1d84e5caf3aba37953930
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-25 00:02:39 +0530

    Password change updated API

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,5 @@
 import User from '../models/user-model.js'
-
+import bcrypt from 'bcryptjs';
 
 export const updateUser = async (req,res,next)=>{
       if(req.user._id!==req.params.id){
@@ -26,6 +26,41 @@ export const updateUser = async (req,res,next)=>{
                 email:updateUser.email
             }
          })
+      }
+      catch(error){
+        next(error);
+      }
+}
+
+export const updatePassword = async(req,res,next)=>{
+     if(req.user._id!==req.params.id){
+           return res.status(200).json({
+            success:false,
+            message:"you can only update your own account"
+           })
+      }
+      try{
+        if(req.body.password){
+            req.body.password=bcrypt.hashSync(req.body.password,10);
+        }
+        else{
+           return res.json({
+                success:false,
+                message:"Password is missing"
+            })
+        }
+        const updatedUser=  await User.findByIdAndUpdate(req.params.id,{
+            $set:{
+                password:req.body.password
+            }
+        },{new:true});
+
+        return res.json({
+            success:true,
+            message:"user updated Password successfully"
+        })
+       
+
       }
       catch(error){
         next(error);
--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { verify_token } from '../middleware/verify-user';
-
+import { verify_token } from '../middleware/verify-user.js';
+import { updateUser,updatePassword } from '../controllers/user-controller.js';
 const router =express.Router();
 
-router.post("/update/id:",verify_token,updateUser);
-
+router.post("/update/:id",verify_token,updateUser);
+router.post("/update/password/:id",verify_token,updatePassword);
 
 export default router;
