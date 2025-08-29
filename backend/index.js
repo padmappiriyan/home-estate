7818d2acc4e689969bab895a951c7364d670f25e
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2025-08-29 23:54:51 +0530

    Backend Updated

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app=express();
 connectDB();
 cloudinaryConnect();
 app.use(cors({
-  origin: "http://localhost:5174", 
+  origin: "http://localhost:5173", 
   credentials: true,               
 }));
 
